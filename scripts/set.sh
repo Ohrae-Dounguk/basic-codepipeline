4f848e7ecb5a2f170d916362b9be713107c7c2be
dulee <dulee@ohrae.biz>
2023-11-28 13:03:07 +0900

    .

--- scripts/set.sh
+++ scripts/set.sh
@@ -4,7 +4,7 @@ REPOSITORY=/home/ubuntu/git/source
 
 cd $REPOSITORY
 
-aws ssm get-parameters-by-path --path /code-pipeline --query Parameters | \
+aws ssm get-parameters-by-path --path /code_pipeline/development --query Parameters | \
 python3 -c '
 import json, sys
 print("\n".join([param["Name"].rsplit("/", 1)[-1]+"="+param["Value"] for param in json.load(sys.stdin)]))
