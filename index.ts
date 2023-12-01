891bcaddebf5c336c8d9d86581d8a3402b4472e0
dulee <dulee@ohrae.biz>
2023-12-01 18:22:00 +0900

    .

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ const app: Application = express();
 const port = process.env.PORT || 5310;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send(`Welcome to Express & TypeScript Server Hello! ${process.env.API_PREFIX} HH ${env.app.apiPrefix} DD HH HHIIIIIIIIHHIIIIII`);
+  res.send(`Welcome to Express & TypeScript Server Hello! ${process.env.API_PREFIX} HH ${env.app.apiPrefix} DD HH HHIIIIIIIIHHIIIIII 그만괴롭혀라`);
 });
 
 app.listen(port, () => {
